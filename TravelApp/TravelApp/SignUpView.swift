e0c3403cd75662ecb327f74d7b63c37d6bb20ee4
Kalyan <pvkalyan427@gmail.com>
2024-05-17 19:08:28 -0400

    Initial Commit/Created view for Initial Screen

---
+++ TravelApp/TravelApp/SignUpView.swift
@@ -0,0 +1,60 @@
+//
+//  ContentView.swift
+//  TravelApp
+//
+//  Created by kalyan . on 5/17/24.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    var body: some View {
+        VStack {
+            Text("Travelisto")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            
+            Image("Logoimg")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 300,height: 300)
+                .padding(.top, 50)
+            
+            Text("Plan your travel anytime, anywhere")
+                .font(.subheadline)
+                .foregroundStyle(.gray)
+                .padding(.top, 10)
+            
+            Spacer()
+                .frame(height: 140)
+            
+            Button(action: {
+                
+            }, label: {
+                Text("Create an account")
+                    .font(.headline)
+                    .foregroundStyle(.white)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(.orange)
+                    .cornerRadius(40)
+                    .padding(.horizontal, 20)
+            })
+            
+            Button(action: {
+                
+            }, label: {
+                Text("Already on Travel? Log In")
+                    .font(.subheadline)
+                    .foregroundStyle(.gray)
+                    .padding(.top, 10)
+            })
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    SignUpView()
+}
---
+++ TravelApp/TravelApp/TravelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelAppApp.swift
+//  TravelApp
+//
+//  Created by kalyan . on 5/17/24.
+//
+
+import SwiftUI
+
+@main
+struct TravelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SignUpView()
+        }
+    }
+}
